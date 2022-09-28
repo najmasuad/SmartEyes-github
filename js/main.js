2b06f7d6d021b115fbd9f8f7f4ed8c01e1d64fcb
najmasuad <suad.najmaali@awacademy.training>
2022-09-29 01:16:00 +0200

    getElemntById not working, fix it?

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("hello from main.js");
+const weekDays = [
+    [new Date(2022, 9, 17, 9, 0), new Date(2022, 9, 17, 9, 30) ],
+    [new Date(2022, 9, 17, 9, 0), new Date(2022, 9, 17, 9, 30) ],
+    [new Date(2022, 9, 17, 9, 0), new Date(2022, 9, 17, 9, 30) ],
+    [new Date(2022, 9, 17, 9, 0), new Date(2022, 9, 17, 9, 30) ],
+];
+
+// dynamically create html
+
+var contents = ``;
+weekDays.forEach( (item) => {
+contents += `<div class="intervalDIV">${item[0].toLocaleTimeString()} - ${item[1].toLocaleTimeString()}</div>` 
+})
+
+console.log("contents is = ",contents);
+// set innerHtml
+document.getElementById('weekdayHours').innerHTML = contents;
